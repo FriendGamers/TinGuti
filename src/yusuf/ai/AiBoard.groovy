2deec613c7c5b3c1a5249f94a46692536f7f0835
Yusuf <md_yusuf000@yahoo.com>
2014-02-11 02:53:29 +0600

    trying

---
+++ src/yusuf/ai/AiBoard.groovy
@@ -0,0 +1,24 @@
+package yusuf.ai
+/**
+ * Created with IntelliJ IDEA.
+ * User: Yusuf
+ * Date: 11/5/13
+ * Time: 11:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class AiBoard {
+    int[] boardValue; //= new int[90];
+    int[] markBoard; //= new int[90];
+    int[][] board; //= new int[10][10];
+
+    public AiBoard() {
+        boardValue = new int[90];
+        markBoard = new int[90];
+        board = new int[10][10];
+
+    }
+
+    static void calculateBoardValue() {
+
+    }
+}
---
+++ src/yusuf/play/Play.groovy
@@ -0,0 +1,15 @@
+package yusuf.play
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Yusuf
+ * Date: 11/5/13
+ * Time: 11:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Play {
+
+    public static void main(String[] argv) {
+
+    }
+}
