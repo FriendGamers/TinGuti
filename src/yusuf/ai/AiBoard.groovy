242057ba91be9cb83cc961e33f6c13947cd26f19
Yusuf Office <mdyusuf@tigerit.com>
2014-03-07 16:50:05 +0600

    setting board value

--- src/yusuf/ai/AiBoard.groovy
+++ src/yusuf/ai/AiBoard.groovy
@@ -6,6 +6,15 @@ package yusuf.ai
  * Time: 11:04 PM
  * To change this template use File | Settings | File Templates.
  */
+/**
+ * Not moved player1 is 1
+ * Moved player1 2
+ * Not moved player2 is 3
+ * Moved player2 is 4
+ * board value 0 draw
+ * board value 1 player1 win
+ * board value 2 player2 win
+**/
 class AiBoard {
     int[] boardValue; //= new int[90];
     int[] markBoard; //= new int[90];
@@ -14,8 +23,13 @@ class AiBoard {
     public AiBoard() {
         boardValue = new int[90];
         markBoard = new int[90];
-        board = new int[10][10];
-
+        board = new int[3][3];
+        board[0][0] = 1;
+        board[0][1] = 1;
+        board[0][2] = 1;
+        board[2][0] = 3;
+        board[2][1] = 3;
+        board[2][2] = 3;
     }
 
     static void calculateBoardValue() {
