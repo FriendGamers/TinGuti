f1545fe6f0bde6bba5a6ba63097338435ec82177
YusufOffice <md.yusuf@tigerit.com>
2015-09-15 15:25:53 +0600

    circle drag

--- src/yusuf/game/GameBoard.groovy
+++ src/yusuf/game/GameBoard.groovy
@@ -1,68 +1,88 @@
 package yusuf.game
 
+import javax.swing.JFrame
 import javax.swing.JPanel
 import java.awt.Color
 import java.awt.Graphics
+import java.awt.Point
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
-import java.awt.event.MouseMotionAdapter
+import java.awt.geom.Ellipse2D
 
 /**
  * Created by YUSUF on 9/15/15.
  */
 class GameBoard extends JPanel {
-    public int x1, x2, y1, y2, r, w, h,xDist,yDist;
-    public static boolean flag = false, pressFlag = false;
+    private final class MouseDrag extends MouseAdapter {
+        private boolean dragging = false;
+        private Point last;
 
-    public r() {
-        setBackground(Color.WHITE);
-        addMouseListener(new MouseAdapter() {
-
-            public void mousePressed(MouseEvent m) {
-//                pressFlag = true;
-                if (r > (int) Math.sqrt(Math.abs(m.getX() - x1) * Math.abs(m.getX() - x1) + Math.abs(m.getY() - y1) * Math.abs(m.getY() - y1))) {
-                    flag = true;
-                    yDist=xDist=x2 = y2 = 0;
-                } else {
-                    x1 = y1 = 0;
-                    r=x2 = y2 = 0;
-                    x1 = m.getX();
-                    y1 = m.getY();
-                }
-                repaint();
+        @Override
+        public void mousePressed(MouseEvent m) {
+            last = m.getPoint();
+            dragging = isInsideEllipse(last);
+            if (!dragging) {
+                x = last.x;
+                y = last.y;
+                width = 0;
+                height = 0;
             }
+            repaint();
+        }
 
-            public void mouseReleased(MouseEvent m) {
-                w = x2 - x1;
-                h = y2 - y1;
-                r = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-                flag = false;
+        @Override
+        public void mouseReleased(MouseEvent m) {
+            last = null;
+            dragging = false;
+            repaint();
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent m) {
+            int dx = m.getX() - last.x;
+            int dy = m.getY() - last.y;
+            if (dragging) {
+                x += dx;
+                y += dy;
+            } else {
+                width += dx;
+                height += dy;
             }
-        });
-        addMouseMotionListener(new MouseMotionAdapter() {
-
-            public void mouseDragged(MouseEvent m) {
-                if (flag && (x2!=0 && y2!=0)) {
-                    xDist=(m.getX()-x2);
-                    yDist=(m.getY()-y2);
-                }
-                x2 = m.getX();
-                y2 = m.getY();
-
-                repaint();
-            }
-        });
+            last = m.getPoint();
+            repaint();
+        }
     }
 
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    private MouseDrag mouseDrag;
+
+    public GameBoard() {
+        setBackground(Color.WHITE);
+        mouseDrag = new MouseDrag();
+        addMouseListener(mouseDrag);
+        addMouseMotionListener(mouseDrag);
+    }
+
+    public boolean isInsideEllipse(Point point) {
+        return new Ellipse2D.Float(x, y, width, height).contains(point);
+    }
+
+    @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (flag) {
-            x1=x1+xDist;
-            y1=y1+yDist;
-            g.drawOval(x1, y1, w, h);
-        } else {
-            g.drawOval(x1, y1, x2 - x1, y2 - y1);
-        }
+        g.drawOval(x, y, width, height);
+    }
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setSize(300, 300);
+        jFrame.add(new GameBoard());
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
 
